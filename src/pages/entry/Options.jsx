d1bf2d7c5a3a194fcbd722ca361232ccde456c9c
vy.ct <vyct@mor.com.vn>
2022-12-23 16:52:10 +0700

    update

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import ScoopOption from './ScoopOption';
-import Row from 'react-bootstrap/Row';
-import ToppingOption from './ToppingOption';
-import AlertBanner from '../common/AlertBanner';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-
-const Options = ({ optionType }) => {
-
+export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
     const [error, setError] = useState(false);
+    const { totals } = useOrderDetails();
 
-    // optionType is 'scoops' or 'toppings'
+    // optionType is 'scoops' or 'toppings
     useEffect(() => {
-        axios.get(`http://localhost:3030/${optionType}`)
-            .then((res) => {
-                setItems(res.data)
-            })
-            .catch(error => {
-                // Do it later
-                setError(true)
-            })
+        axios
+            .get(`http://localhost:3030/${optionType}`)
+            .then((response) => setItems(response.data))
+            .catch((error) => setError(true));
     }, [optionType]);
 
-    // TODO: replace `null` with ToppingOption when available
-    const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+    if (error) {
+        // @ts-ignore
+        return <AlertBanner />;
+    }
+
+    const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+    const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
     const optionItems = items.map((item) => (
         <ItemComponent
@@ -34,14 +37,14 @@ const Options = ({ optionType }) => {
         />
     ));
 
-    if (error) {
-        // @ts-ignore
-        return <AlertBanner />;
-    }
-
     return (
-        <Row>{optionItems}</Row>
-    )
+        <>
+            <h2>{title}</h2>
+            <p>{formatCurrency(pricePerItem[optionType])} each</p>
+            <p>
+                {title} total: {formatCurrency(totals[optionType])}
+            </p>
+            <Row>{optionItems}</Row>
+        </>
+    );
 }
-
-export default Options
